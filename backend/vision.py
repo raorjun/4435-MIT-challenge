e5422a0cabbf1fdb8c2a9c89e11340e1328ea185
Arjun Rao <rao.rjun@gmail.com>
2026-03-16 21:09:05 -0400

    vision prompt

--- backend/vision.py
+++ backend/vision.py
@@ -1,2 +1,23 @@
-# Claude API vision calls
-# Takes a base64 image, returns a scene description string for TTS
+import os
+import PIL.Image as Image
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# config for vlm
+API_KEY = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=API_KEY)
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+def get_spatial_narration(image_bytes, destination, map_context = None):
+    img = Image.open(image_bytes)
+    system_instructions = "prompt" #TODO: prompt
+    context = f"\nMap context: {map_context}" if map_context else ""
+    user_ask = f"Aditi wants to go to: {destination}"
+    prompt = system_instructions + context + user_ask
+    response = model.generate_content([prompt, img])
+    return response.text
+
+
+
