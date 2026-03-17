0e5c4adff8780b0d4af231978512a0b6639f84c0
Arjun Rao <rao.rjun@gmail.com>
2026-03-17 17:15:29 -0400

    updated python

--- backend/vision.py
+++ backend/vision.py
@@ -1,23 +1,29 @@
-import os
-import PIL.Image as Image
-import google.generativeai as genai
+import io
+from google import genai
+from google.genai import types
+from PIL import Image
 from dotenv import load_dotenv
 
 load_dotenv()
+# since api key is named GOOGLE_API_KEY, genai will autofill it from the environment variable
+client = genai.Client()
 
-# config for vlm
-API_KEY = os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
 
-def get_spatial_narration(image_bytes, destination: str, map_context = None) -> str:
-    img = Image.open(image_bytes)
-    system_instructions = "prompt" #TODO: prompt
-    context = f"\nMap context: {map_context}" if map_context else ""
-    user_ask = f"Aditi wants to go to: {destination}"
-    prompt = system_instructions + context + user_ask
-    response = model.generate_content([prompt, img])
+def get_spatial_narration(image_bytes, destination, map_context = None):
+    img = Image.open(io.BytesIO(image_bytes))
+    system_instruction = "" #TODO
+
+    user_ask = f"Aditi wants to go to: {destination}."
+    if map_context:
+        user_ask += f"\nAdditional Map/GPS Context: {map_context}"
+
+    response = client.models.generate_content(
+        model="gemini-1.5-flash",
+        contents=[img, user_ask],
+        config=types.GenerateContentConfig(
+            system_instruction=system_instruction,
+            temperature=0.4,  # Lower temperature for more factual, less "creative" directions
+        )
+    )
+
     return response.text
-
-
-
