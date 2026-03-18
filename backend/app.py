20ce742359bcba51ae151a7d65ae52246a57770a
Arjun Rao <rao.rjun@gmail.com>
2026-03-18 00:09:41 -0400

    backend commit

--- backend/app.py
+++ backend/app.py
@@ -2,19 +2,22 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import io
 from vision import get_spatial_narration
-# map context for
 
 app = Flask(__name__)
 CORS(app) # needed for cross domain connection
 
 @app.route("/vision/navigate", methods=["POST"])
 def navigate():
-    if "image" not in request.files:
-        return jsonify({"error": "no image"}), 400
+    image_file = request.files.get('image')
+    destination = request.form.get('destination', 'the nearest exit')
 
-    # implement map context and then call the vlm
+    if not image_file:
+        return jsonify({"error": "No image provided"}), 400
+    image_bytes = image_file.read()
 
+    narration = get_spatial_narration(image_bytes, destination)
+    return jsonify({"narration": narration})
 
 if __name__ == '__main__':
-    print('Starting Steplight Backend API...')
+    print('Starting Backend API...')
     app.run(host='0.0.0.0', port=5000, debug=True)
